36801e3b57ff2d1e0ba9e732d0e37e494a8c9ac6
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-05-01 15:45:29 +0530

    Assignment

---
+++ React Introduction/react-mobile-os/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="a">
+        <h1>Mobile Operating system</h1>
+        <ul>
+          <li>Android</li>
+          <li>BlackBerry</li>
+          <li>iPhone</li>
+          <li>Windows Phone</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Mobile Operating system</h1>
+        <ul className="b">
+          <li>Android</li>
+          <li>BlackBerry</li>
+          <li>iPhone</li>
+          <li>Windows Phone</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
