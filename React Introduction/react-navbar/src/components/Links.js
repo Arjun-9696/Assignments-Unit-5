0e45a313db8a08d0e8e114baa5ccb3676b4167b5
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-05-01 16:53:00 +0530

    Assignments

---
+++ React Introduction/react-navbar/src/components/Links.js
@@ -0,0 +1,22 @@
+import "./Links.css"
+function Links() {
+  return (
+    <>
+      <div className="navbar">
+        <div>
+          <h5>LOGOBAKERY</h5>
+        </div>
+        <div className="links">
+          <button href="#" className="">Services</button>
+          <button href="#" className="">Projects</button>
+          <button href="#" className="">About</button>
+        </div>
+
+        <button className="btn">Contact</button>
+        
+      </div>
+    </>
+  );
+}
+
+export default Links;
